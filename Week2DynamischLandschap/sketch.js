2340c0156cfcc3bf2f35219b24c35f540fc90e9d
MR_artemka <artem20051205@gmail.com>
2025-09-09 14:40:07 +0200

    dag 2

---
+++ Week2DynamischLandschap/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(1100, 1100);
+}
+
+function draw() {
+  background(220);
+fill("red")
+strokeWeight(0)
+rect(620, 100, 220,40)
+fill("#1b3c52")
+rect(620, 100, 20,20)
+rect(740, 100, 100,20)
+rect(720, 200, 80,20)
+fill("#ffc387ff")
+rect(620, 140 ,120 ,100)
+rect(720, 160 ,100 ,40)
+rect(740, 220 ,20 ,20)
+fill(0)
+rect(720, 140, 20,40)
+rect(740, 180, 20,20)
+rect(720, 200, 80,20)
+}
