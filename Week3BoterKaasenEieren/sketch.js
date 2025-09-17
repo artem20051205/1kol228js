e96616071ecf856aebd7f2097eec503e10ca2df9
MR_artemka <artem20051205@gmail.com>
2025-09-17 14:58:03 +0200

    eind 17.09

--- Week3BoterKaasenEieren/sketch.js
+++ Week3BoterKaasenEieren/sketch.js
@@ -25,9 +25,9 @@ function draw() {
   rect(75,75,250,250,50)
   fill(a1)
   rect(375,75,250,250,50)
-  fill(a2),10
+  fill(a2)
   rect(675,75,250,250,50)
-  fill(b0),10
+  fill(b0)
   rect(75,375,250,250,50)
   fill(b1)
   rect(375,375,250,250,50)
@@ -43,71 +43,58 @@ function draw() {
   if (b_color == 'blue'){
     textSize(42)
   text("Blue's trun",42,42,)
-}
-  else{
+}else{
     textSize(42)
   text("Red's trun",42,42,)
   }
   if (a1=='blue'&& a2=='blue'&& a0=='blue'){
     turn = 21
-  }
-  if (b1=='blue'&& b2=='blue'&& b0=='blue'){
+  }if (b1=='blue'&& b2=='blue'&& b0=='blue'){
     turn = 21
-  }
-  if (c1=='blue'&& c2=='blue'&& c0=='blue'){
+  }if (c1=='blue'&& c2=='blue'&& c0=='blue'){
     turn = 21
-  }
-  if (a1=='blue'&& b1=='blue'&& c1=='blue'){
+  }if (a1=='blue'&& b1=='blue'&& c1=='blue'){
     turn = 21
-  }
-  if (a2=='blue'&& b2=='blue'&& c2=='blue'){
+  }if (a2=='blue'&& b2=='blue'&& c2=='blue'){
     turn = 21
-  }
-  if (a0=='blue'&& b0=='blue'&& c0=='blue'){
+  }if (a0=='blue'&& b0=='blue'&& c0=='blue'){
     turn = 21
-  }
-  if (a1=='red'&& a2=='red'&& a0=='red'){
+  }if (a1=='red'&& a2=='red'&& a0=='red'){
     turn = 22
-  }
-  if (b1=='red'&& b2=='red'&& b0=='red'){
+  }if (b1=='red'&& b2=='red'&& b0=='red'){
     turn = 22
-  }
-  if (c1=='red'&& c2=='red'&& c0=='red'){
+  }if (c1=='red'&& c2=='red'&& c0=='red'){
     turn = 22
-  }
-    if (a1=='red'&& b1=='red'&& c1=='red'){
+  }if (a1=='red'&& b1=='red'&& c1=='red'){
     turn = 22
-  }
-  if (a2=='red'&& b2=='red'&& c2=='red'){
+  }if (a2=='red'&& b2=='red'&& c2=='red'){
     turn = 22
-  }
-  if (a0=='red'&& b0=='red'&& c0=='red'){
+  }if (a0=='red'&& b0=='red'&& c0=='red'){
     turn = 22
-  }
-  if (a0=='red'&& b1=='red'&& c2=='red'){
+  }if (a0=='red'&& b1=='red'&& c2=='red'){
     turn = 22
-  }
-  if (a2=='red'&& b1=='red'&& c0=='red'){
+  }if (a2=='red'&& b1=='red'&& c0=='red'){
     turn = 22
-  }
-  if (a0=='blue'&& b1=='blue'&& c2=='blue'){
+  }if (a0=='blue'&& b1=='blue'&& c2=='blue'){
     turn = 21
-  }
-  if (a2=='blue'&& b1=='blue'&& c0=='blue'){
+  }if (a2=='blue'&& b1=='blue'&& c0=='blue'){
     turn = 21
-  }
-  if (turn == 21){
+  }if (turn == 21){
 a0_block=0;b0_block=0;c0_block=0;
 a1_block=0;b1_block=0;c1_block=0;
 a2_block=0;b2_block=0;c2_block=0;
 fill(255)
 textSize(140)
-strokeWeight(10)
+strokeWeight(5)
 stroke(0)
 fill('#0099ffff')
 text('Blue Win',210,500)
 fill(255)
-text('Restart',240,650)
+if (turn >= 10 && mouseX >= 260 && mouseX <= 710 && mouseY >= 510 && mouseY <= 670){
+stroke('red')
+  strokeWeight(10)
+}
+text('Restart',260,650)
 strokeWeight(2)
 }
   if (turn == 22){
@@ -121,18 +108,27 @@ stroke(0)
 fill('#e60000ff')
 text('Red Win',210,500)
 fill(255)
-text('Restart',240,650)
+if (turn >= 10 && mouseX >= 260 && mouseX <= 710 && mouseY >= 510 && mouseY <= 670){
+stroke('red')
+  strokeWeight(10)
+}
+text('Restart',260,650)
 strokeWeight(2)
 }
   if (turn == 10){
 a0_block=0;b0_block=0;c0_block=0;
 a1_block=0;b1_block=0;c1_block=0;
 a2_block=0;b2_block=0;c2_block=0;
-fill(255)
+fill(42)
 textSize(140)
-text('Draw',280,500)
+text('Draw',330,500)
 strokeWeight(10)
-text('Restart',240,650)
+fill(255)
+if (turn >= 10 && mouseX >= 260 && mouseX <= 710 && mouseY >= 510 && mouseY <= 670){
+stroke('red')
+  strokeWeight(10)
+}
+text('Restart',260,650)
 strokeWeight(0)
 }
 }
@@ -219,9 +215,9 @@ function mousePressed() {
     c2_block = 0
   }
   if (turn >= 10 && mouseX >= 240 && mouseX <= 760 && mouseY >= 510 && mouseY <= 670) {
-    a0_block = 1;b0_block = 1;c0_block = 1;
-    a1_block = 1;b1_block = 1;c1_block = 1;
-    a2_block = 1;b2_block = 1;c2_block = 1;
+    a0_block = 1; b0_block = 1; c0_block = 1;
+    a1_block = 1; b1_block = 1; c1_block = 1;
+    a2_block = 1; b2_block = 1; c2_block = 1;
     a0 = 50;b0 = 50;c0 = 50;
     a1 = 50;b1 = 50;c1 = 50;
     a2 = 50;b2 = 50;c2 = 50;
